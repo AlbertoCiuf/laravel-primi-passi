d7966012ffe16b58b0ef03bc56c541a2d79c3b9d
AlbertoCiuf <89313766+AlbertoCiuf@users.noreply.github.com>
2022-01-20 16:08:27 +0100

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Laravel HOME</title>
+</head>
+<body>
+  <h1>Home</h1>
+
+  <h3>Visita la pagina <a href="#">About Me </a> per saperne di più...</h3>
+
+</body>
+</html>
